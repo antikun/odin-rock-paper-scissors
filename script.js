86a5b3b257d0ba31dd2c5a9d0f4e2db648a762f4
antikun <121240743+antikun@users.noreply.github.com>
2023-01-18 21:55:49 +0200

    Add the playRound function
    Created the playRound function that.. well, plays a round of the game.
    Seems like it's maybe too verbose and not very neat, but... that's what
    I came up with.
    It works as intended but I can't say I'm too happy with it, will try to
    make it cleaner.

--- script.js
+++ script.js
@@ -4,13 +4,29 @@ console.log(choice)
 function getComputerChoice(choice) {
     console.log(arguments)
     if (choice === 1) {
-        console.log("Rock");
-        return "Rock";
+        console.log("rock");
+        return "rock";
     } else if (choice === 2) {
-        console.log("Paper");
-        return "Paper";
+        console.log("paper");
+        return "paper";
     } else {
-        console.log("Scissors");
-        return "Scissors";
+        console.log("scissors");
+        return "scissors";
+    }
+}
+
+const computerSelection = getComputerChoice(choice);
+
+const playerSelection = "rock";
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "scissors" && computerSelection === "paper" ||
+        playerSelection === "paper" && computerSelection === "rock") {
+        return `You win! ${playerSelection} beats ${computerSelection}.`
+    } else if (playerSelection === computerSelection) {
+        return `It's a draw!`
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}.`
     }
 }
