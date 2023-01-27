1b30581fb4c266ed030911931de01a7b6b3f7caf
antikun <121240743+antikun@users.noreply.github.com>
2023-01-27 19:51:24 +0200

    Rework rest of the old code

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const computerScore = document.querySelector(".computer-wins");
 const tieScore = document.querySelector(".ties");
 
 const playTo = document.querySelector("#play-to")
+let winningScore = parseInt(playTo.value);
 
 function getComputerChoice() {
     const randomNum = Math.floor(Math.random() * 3) + 1;
@@ -42,57 +43,50 @@ function playRound(player, computer) {
     }
 }
 
+let isGameOver = false;
+const win = document.querySelector(".win");
 
-// function playRound(playerSelection, computerSelection) {
-//     computerSelection = getComputerChoice(randomNum);
-//     playerSelection = prompt("Rock, paper, or scissors? Make your choice").toLowerCase();
-//     const capitalize = (str) => {
-//         let firstLetter = str.slice(0, 1).toUpperCase();
-//         let restOfStr = str.slice(1).toLowerCase()
-//         return firstLetter + restOfStr;
-//     }
-//     while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-//         playerSelection = prompt("Invalid entry. Please, choose rock, paper, or scissors.").toLowerCase();
-//     }
-//     if (playerSelection === "rock" && computerSelection === "scissors" ||
-//         playerSelection === "scissors" && computerSelection === "paper" ||
-//         playerSelection === "paper" && computerSelection === "rock") {
-//         playerWins++;
-//         console.log(`You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}.`)
-//         return `You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}.`;
-//     } else if (playerSelection === computerSelection) {
-//         console.log(`It's a draw!`)
-//         return `It's a draw!`;
-//     } else {
-//         computerWins++;
-//         console.log(`You lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}.`)
-//         return `You lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}.`;
-//     }
-// }
+function game() {
+    if (!isGameOver) {
+        playRound(playerSelection, computerSelection);
+    } if (playerScore.textContent == winningScore || computerScore.textContent == winningScore) {
+        isGameOver = true;
+        win.style.color = "green";
+    }
+}
 
+rock.addEventListener("click", () => {
+    playerSelection = "rock";
+    computerSelection = getComputerChoice();
+    game();
+})
 
-// function game(num) {
-//     for (let i = 0; i < num; i++) {
-//         playRound(playerSelection, computerSelection);
-//     }
-//     while (playerWins === computerWins) {
-//         console.log("It's a draw! Play one more round!")
-//         playRound(playerSelection, computerSelection)
-//     }
-//     console.log("*****************************");
-//     if (playerWins > computerWins) {
-//         console.log("Congrats! You won the game!");
-//     } else {
-//         console.log("Ah, too bad! You lost the game!");
-//     }
-//     console.log("*****************************");
-// }
+paper.addEventListener("click", () => {
+    playerSelection = "paper";
+    computerSelection = getComputerChoice();
+    game();
+})
 
-// const gameStart = prompt("A game of Rock, Paper, Scissors? Type in \"start\"");
-// if (gameStart == null) {
-//     console.log("Oh, okay. Maybe next time!")
-// } else if (gameStart.toLowerCase() === "start") {
-//     game(5);
-// } else {
-//     console.log("Mistyped \"start\"? Try again?")
-// }
+scissors.addEventListener("click", () => {
+    playerSelection = "scissors";
+    computerSelection = getComputerChoice();
+    game();
+})
+
+function resetGame() {
+    isGameOver = false;
+    playerWins = 0;
+    computerWins = 0;
+    ties = 0;
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+    tieScore.textContent = 0;
+    win.style.color = "black"
+}
+
+resetBtn.addEventListener("click", resetGame);
+
+playTo.addEventListener("change", () => {
+    winningScore = parseInt(playTo.value);
+    resetGame();
+})
