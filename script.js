db8f37f4ebb84e4e86d8ee2c77687ccbc844fa18
antikun <121240743+antikun@users.noreply.github.com>
2023-02-02 00:11:30 +0200

    Finish up
    I tried to make the UX for mobile users better, but failed miserably.
    I'm leaving it as it was before the last attempts at making the page
    fit the screen perfectly without any need for scrolling. I guess I'll
    have to start using remote debugging.

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// --vh 
-
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty("--vh", `${vh}px`);
-window.addEventListener("resize", () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `#{vh}px`);
-})
-
-// --vh 
-
 const computer = {
     score: 0,
     btn: [document.querySelector("#computer-rock"), document.querySelector("#computer-paper"), document.querySelector("#computer-scissors")],
